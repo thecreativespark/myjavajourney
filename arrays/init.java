467b2e302c5d7a86229fdf6d1c04e823ce9f6420
tcs <thecreativespark@proton.me>
2024-07-06 19:55:00 +0530

    different ways to init an array in Java

---
+++ arrays/init.java
@@ -0,0 +1,9 @@
+public class init{
+	public static void main(String [] args){
+		// init using new
+		int[] array1 = new int[10];
+
+		// init using initializer list
+		int[] array2 = {1,2,3,4,5,6,7,8,9,0};
+	}
+}
